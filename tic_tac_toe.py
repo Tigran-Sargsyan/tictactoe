cb3923429272d51857b0333e71fc4bf16baae5e2
Tigran Sargsyan <99718203+Tigran-Sargsyan@users.noreply.github.com>
2023-03-07 00:00:11 +0400

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -7,53 +7,55 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt5.QtGui import QFont, QIcon
 
+
 class ChooseGameMode(QWidget):
     def __init__(self):
         super().__init__()
 
         self.window_width = 502
         self.window_height = 570
-        
-        #Geometry of the window
+
+        # Geometry of the window
         self.setGeometry(500, 100, 462, 570)
         self.setMinimumSize(self.window_width, self.window_height)
         self.setMaximumSize(self.window_width, self.window_height)
-        
-        #Styling of the window
+
+        # Styling of the window
         self.setWindowTitle("TicTacToe")
         self.setWindowIcon(QIcon("icon.jpg"))
         self.setStyleSheet('background-color: #adc9c3;')
-        
-        #A label to display the message
+
+        # A label to display the message
         welcome = QtWidgets.QLabel('Welcome to TicTacToe!', self)
         welcome.setGeometry(40, 10, 400, 70)
         welcome.setStyleSheet("QLabel{font-size: 20pt;color: green;}")
-        
-        #A label to choose the game mode
+
+        # A label to choose the game mode
         label = QtWidgets.QLabel('Choose the game mode', self)
         label.setGeometry(45, 70, 400, 140)
         label.setStyleSheet("QLabel{font-size: 18pt;}")
 
-        #Buttons for the game modes
+        # Buttons for the game modes
         self.two_players_button = QtWidgets.QPushButton('2 Players', self, width=200, height=150)
         self.two_players_button.clicked.connect(self.two_players_selected)
         self.two_players_button.move(165, 200)
         self.two_players_button.setStyleSheet("QPushButton{font-size: 19pt;}")
-        
+
         self.vs_computer_button = QtWidgets.QPushButton('VS Computer', self, width=200, height=150)
         self.vs_computer_button.clicked.connect(self.vs_computer_selected)
         self.vs_computer_button.move(130, 300)
         self.vs_computer_button.setStyleSheet("QPushButton{font-size: 19pt;}")
 
     def two_players_selected(self):
-        #Window.mode(Window, 1)
+        # Window.mode(Window, 1)
         Window.game_mode = '2 player'
         self.close()
 
     def vs_computer_selected(self):
-        #Window.mode(Window, 2)
+        # Window.mode(Window, 2)
         Window.game_mode = 'vs computer'
         self.close()
+
 
 class Window(QMainWindow):
     """
@@ -66,27 +68,27 @@ class Window(QMainWindow):
 
         self.window_width = 502
         self.window_height = 570
-        #Geometry of the window
+        # Geometry of the window
         self.setGeometry(500, 100, 462, 570)
         self.setMinimumSize(self.window_width, self.window_height)
         self.setMaximumSize(self.window_width, self.window_height)
-        
+
         self.setWindowTitle("TicTacToe")
         self.setWindowIcon(QIcon("icon.jpg"))
 
         self.move_counter = 0  # A counter to determine whose move it is
         self.flag = True  # A flag to tell if a game is still in process
-        #self.game_mode = 0  # A variable for holding the mode of the game (1: 2Player, 2:vs Computer)
+        # self.game_mode = 0  # A variable for holding the mode of the game (1: 2Player, 2:vs Computer)
 
         self.board = list(range(9))  # Defining a list with values [0,8] for storing X-s and O-s
 
         self.win_lst = []  # Defining a list to hold the winning squares
 
-        #Counting how many times has each player won for showing it in the interface
+        # Counting how many times has each player won for showing it in the interface
         self.winX = 0
         self.winY = 0
 
-        self.initUI()  #A function for the UI of the window
+        self.initUI()  # A function for the UI of the window
 
     def initUI(self):
         """Creating buttons,setting geometry,text in the buttons,styling"""
@@ -101,21 +103,21 @@ class Window(QMainWindow):
         self.btn8 = QtWidgets.QPushButton(self)
         self.btn9 = QtWidgets.QPushButton(self)
 
-        self.btn10 = QtWidgets.QPushButton(self)  #btn10 is clicked when the user wants to play again
+        self.btn10 = QtWidgets.QPushButton(self)  # btn10 is clicked when the user wants to play again
         self.btn10.setText("Play again")
         self.btn10.setFont(QFont('Times', 28))
 
-        #Initializing x coordinates
+        # Initializing x coordinates
         x1 = 25
         x2 = x1 + 151
         x3 = x2 + 151
 
-        #Initializng y coordinates
+        # Initializng y coordinates
         y1 = 10
         y2 = 10 + 101
         y3 = y2 + 101
 
-        #Initializing width and height
+        # Initializing width and height
         width = 150
         height = 100
 
@@ -129,9 +131,9 @@ class Window(QMainWindow):
         self.btn8.setGeometry(x2, y3, width, height)
         self.btn9.setGeometry(x3, y3, width, height)
 
-        self.btn10.setGeometry(0, 430, self.window_width+5, 70)
+        self.btn10.setGeometry(0, 430, self.window_width + 5, 70)
 
-        #Creating a list of buttons for further easier use
+        # Creating a list of buttons for further easier use
         self.button_list = [
             self.btn1,
             self.btn2,
@@ -144,57 +146,56 @@ class Window(QMainWindow):
             self.btn9
         ]
 
-        #Setting the text and the size of the text of squares
+        # Setting the text and the size of the text of squares
         for button in self.button_list:
             button.setText("")
             button.setFont(QFont('Times', 30))
 
-        self.label = QtWidgets.QLabel(self)  #Label to tell whose turn to play is at the moment
+        self.label = QtWidgets.QLabel(self)  # Label to tell whose turn to play is at the moment
         self.label.setGeometry(290, 350, 200, 60)
         self.label.setText("X's turn!")
         self.label.setStyleSheet("QLabel{font-size: 27pt;}")
-        
-        self.player1 = QtWidgets.QLabel(self)  #Label to show the player1  
-        self.player2 = QtWidgets.QLabel(self)  #Label to show the player2        
-        
+
+        self.player1 = QtWidgets.QLabel(self)  # Label to show the player1
+        self.player2 = QtWidgets.QLabel(self)  # Label to show the player2
+
         self.player1.setStyleSheet("QLabel{font-size: 18pt;}")
-        self.player2.setStyleSheet("QLabel{font-size: 18pt;}") 
-        
-        self.score_1 = QtWidgets.QLabel(self)  #Label to show the score of player1
+        self.player2.setStyleSheet("QLabel{font-size: 18pt;}")
+
+        self.score_1 = QtWidgets.QLabel(self)  # Label to show the score of player1
         self.score_1.setText("0")
         self.score_1.setStyleSheet("QLabel{font-size: 18pt;}")
 
-        
-        self.score_2 = QtWidgets.QLabel(self)  #Label to show the score of player2
+        self.score_2 = QtWidgets.QLabel(self)  # Label to show the score of player2
         self.score_2.setText("0")
         self.score_2.setStyleSheet("QLabel{font-size: 18pt;}")
-    
+
         player_lbl_width = 130
         player_lbl_height = 55
         player_lbl_x = 10
         player1_lbl_y = 320
         player2_lbl_y = player1_lbl_y + 50
-        
+
         if self.game_mode == '2 player':
             self.player1.setText("Player1: ")
             self.player1.setGeometry(player_lbl_x, player1_lbl_y, player_lbl_width, player_lbl_height)
             print(1)
             self.score_1.setGeometry(150, 329, 45, 40)
-            print(2)            
+            print(2)
             self.player2.setText("Player2: ")
             self.player2.setGeometry(player_lbl_x, player2_lbl_y, player_lbl_width, player_lbl_height)
             self.score_2.setGeometry(150, 379, 45, 40)
-        
+
         elif self.game_mode == 'vs computer':
             self.player1.setText("You:")
             self.player1.setGeometry(player_lbl_x, player1_lbl_y, player_lbl_width, player_lbl_height)
             self.score_1.setGeometry(200, 329, 75, 40)
             self.player2.setText("Computer:")
-            self.player2.setGeometry(player_lbl_x, player2_lbl_y, player_lbl_width+35, player_lbl_height)
+            self.player2.setGeometry(player_lbl_x, player2_lbl_y, player_lbl_width + 35, player_lbl_height)
             self.score_2.setGeometry(200, 379, 75, 40)
-            self.square_list = list(range(9))  #For logic when playing with computer
-            
-        #Styling
+            self.square_list = list(range(9))  # For logic when playing with computer
+
+        # Styling
         self.setStyleSheet('background-color: #adc9c3;')
         self.btn1.setStyleSheet("QPushButton {color: #ff0000;}")
         self.btn2.setStyleSheet("QPushButton {color: #ff0000;}")
@@ -207,7 +208,7 @@ class Window(QMainWindow):
         self.btn9.setStyleSheet("QPushButton {color: #ff0000;}")
         self.btn10.setStyleSheet("background-color: #4b7173")
 
-        #Clicking the buttons
+        # Clicking the buttons
         self.btn1.clicked.connect(lambda: self.click(self.btn1, 0))
         self.btn2.clicked.connect(lambda: self.click(self.btn2, 1))
         self.btn3.clicked.connect(lambda: self.click(self.btn3, 2))
@@ -263,7 +264,7 @@ class Window(QMainWindow):
             self.win(self.board, "O")
 
         print(self.board)
-        self.move_counter += 1  #incrementing the counter after each move
+        self.move_counter += 1  # incrementing the counter after each move
 
     def vsCmp(self, btn, pos):
         """Manages the game process when playing in vs Computer mode and a button on the board is clicked
@@ -342,7 +343,7 @@ class Window(QMainWindow):
                 pick = int(threat_square)
                 print('picked square: ', pick)
                 # TODO
-            else:  #self.square_list:
+            else:  # self.square_list:
                 pick = random.choice(self.square_list)
 
         print('pick: ', pick)
@@ -350,10 +351,10 @@ class Window(QMainWindow):
 
     def check_win_move(self, board):
         """Checks if there is a winning move on each line
-        
+
         Args:
             board (ndarray): A 3x3 ndarray representing the board
-        
+
         Returns:
             row_win_square (str): A square in a row where you can put 'O' and win
             col_win_square (str): A square in a column where you can put 'O' and win
@@ -387,10 +388,10 @@ class Window(QMainWindow):
 
         Args:
             line (ndarray, 1D): A row, column or diagonal to check
-        
+
         Returns:
             line[j] (str): The string number (location) in the board
-            False (bool): If no winning move has been found 
+            False (bool): If no winning move has been found
 
         """
 
@@ -437,13 +438,13 @@ class Window(QMainWindow):
 
     def check_line_threat(self, line):
         """A function to check the row, column and diagonal threats
-        
+
         Args:
             line (ndarray): row, column or diagonal
 
         Returns:
             line[j] (int): threat square
-            False (bool): A boolean value if there is no threat 
+            False (bool): A boolean value if there is no threat
 
         """
 
@@ -486,7 +487,7 @@ class Window(QMainWindow):
 
     def check_horizontal(self, board):
         """Helper of win() that checks horizontals
-        
+
         Args:
             board (ndarray): The 3x3 game board
 
@@ -502,10 +503,10 @@ class Window(QMainWindow):
 
     def check_vertical(self, board):
         """Helper of win() that checks horizontals
-        
+
         Args:
             board (ndarray): The 3x3 game board
-        
+
         Returns:
             True (bool): If the winning condition on the vertical was achieved
 
@@ -519,7 +520,7 @@ class Window(QMainWindow):
 
     def check_diagonal(self, lst):
         """Helper of win() that checks horizontals
-        
+
         Args:
             board (ndarray): The 3x3 game board
 
@@ -557,7 +558,7 @@ class Window(QMainWindow):
         self.square_list = list(range(9))
         self.label.setText("X's turn!")
 
-        #Clearing and reseting
+        # Clearing and reseting
         if self.win_lst:
             for i in range(3):
                 eval(f"self.btn{str(self.win_lst[i] + 1)}").setStyleSheet("QPushButton {color: #ff0000;}")
@@ -566,12 +567,14 @@ class Window(QMainWindow):
         self.win_lst = []
         self.flag = True
 
+
 def main():
     app_ = QApplication([])
     choose_game_mode = ChooseGameMode()
     choose_game_mode.show()
     app_.exec_()
     game()
+
 
 def game():
     app = QApplication(sys.argv)
